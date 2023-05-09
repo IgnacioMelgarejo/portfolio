354f58938dabe7ad7c2539f68e70b1b0c0f3534d
IgnacioMelgarejo <melgarejonacho@hotmail.com>
2023-05-08 21:38:27 -0300

    Links in the works photos

--- src/components/Work.js
+++ src/components/Work.js
@@ -30,20 +30,25 @@ const Work = () => {
             </a>
           </div>
           {/*image*/}
-          <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
-            {/*overbyo */}
-            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-            {/*img */}
-            <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt='' />
-            {/*preTitel */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-              <span className='text-gradient'>Play Masters</span>
+          <a href='https://github.com/IgnacioMelgarejo/PI-Dogs-FT30b'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+              {/*overbyo */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/*img */}
+
+              <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt='' />
+
+              {/*preTitel */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Play Masters</span>
+              </div>
+
+              {/*titel */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3-1 text.white'>Games app</span>
+              </div>
             </div>
-            {/*titel */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-              <span className='text-3-1 text.white'>Games app</span>
-            </div>
-          </div>
+          </a>
         </motion.div>
         <motion.div
           variants={fadeIn('left', 0.2)}
@@ -52,36 +57,41 @@ const Work = () => {
           viewport={{ once: true, amount: 0.3 }}
           className='flex-1 flex flex-col gap-y-10'>
           {/*image2*/}
-          <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
-            {/*overbyo */}
-            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-            {/*img */}
-            <img className='group-hover:scale-125 transition-all duration-500' src={img2} alt='' />
-            {/*preTitel */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-              <span className='text-gradient'>Doggies</span>
+          <a href='https://github.com/IgnacioMelgarejo/Games-Proyect'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+              {/*overbyo */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/*img */}
+              <img className='group-hover:scale-125 transition-all duration-500' src={img2} alt='' />
+              {/*preTitel */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Doggies</span>
+              </div>
+              {/*titel */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3-1 text.white'>App with dog breed information</span>
+              </div>
             </div>
-            {/*titel */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-              <span className='text-3-1 text.white'>App with dog breed information</span>
-            </div>
-          </div>
+          </a>
           {/*image3*/}
-          <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
-            {/*overbyo */}
-            <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-            {/*img */}
-            <img className='group-hover:scale-125 transition-all duration-500' src={img3} alt='https://tv.laruinarecords.cl/' />
-            {/*preTitel */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-              <span className='text-gradient'>La Ruina</span>
-            </div>
-            {/*titel */}
-            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-              <span className='text-3-1 text.white'>Music app</span>
-            </div>
-          </div>
+          <a href='https://tv.laruinarecords.cl/'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-x1'>
+              {/*overbyo */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/*img */}
 
+              <img className='group-hover:scale-125 transition-all duration-500' src={img3} alt='https://tv.laruinarecords.cl/' />
+
+              {/*preTitel */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>La Ruina</span>
+              </div>
+              {/*titel */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3-1 text.white'>Music app</span>
+              </div>
+            </div>
+          </a>
         </motion.div>
       </div>
     </div>
